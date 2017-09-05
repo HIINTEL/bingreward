45c685087ca08cad9519a0e593680d5d1f44f659
Kenney He <kenneyhe@gmail.com>
2017-09-05 12:04:26 -0700

    nosetests run explicitly fix

---
+++ test/unittest.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python -B
+
+import unittest
+import subprocess
+
+"""
+  Testing bing reward with configuration files below
+"""
+class TestBing(unittest.TestCase):
+  def test_assert(self):
+      cmd = "ls config.xml"
+      cmds = cmd.split()
+      status = subprocess.check_call(cmds)
+      self.assertEqual(status, 0, "no config.xml file")
+  def test_configfile(self):
+      cmd = "./main.py -f config.xml"
+      cmds = cmd.split()
+      status = subprocess.check_call(cmds)
+      self.assertEqual(status, 0, "fail to run config.xml")
+
+if __name__ == '__main__':
+  unittest.main(verbosity=2)
