aa28cbcf01e635ec52a807b3263de05775c01beb
Kenney He <kenneyhe@gmail.com>
2017-09-08 22:09:13 -0700

    add proxy tag with protocols, url, login, password

--- test/configtest.py
+++ test/configtest.py
@@ -67,7 +67,10 @@ class TestConfig(unittest.TestCase):
                 <ua_mobile>mozilla/5.0 (iphone; cpu iphone os 7_0_2 like mac os x) applewebkit/537.51.1 (khtml, like gecko) version/7.0 mobile/11a501 safari/9537.53</ua_mobile>
             </account>
         </accounts>
-
+        <proxy protocols="http"
+               url="www.bing.com"
+               login="xxx"
+               password="yyy" />
         <events>
             <onError>
                 <retry interval="5" salt="3.5" count="1" />
@@ -343,6 +346,7 @@ class TestConfig(unittest.TestCase):
         self.assertIsNotNone(ConfigError("ok"), "should return exception")
         self.assertIsNotNone(Config.Proxy(), "should return class")
         self.assertIsNotNone(Config.Event.Specifier(), "should return class")
+        self.assertIsNotNone(Config.EventAccount(), "should return class")
         self.assertIsNotNone(Config.Event.Notify(), "should return class")
         self.assertIsNotNone(str(Config.Event.IfStatement()), "should return class")
         dist = os.path.join(os.path.dirname(__file__), "..", "config.xml.dist")
