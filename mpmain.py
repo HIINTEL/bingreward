a06b405e7f92a15d77ea985c856f64697a4bedab
Kenney He <kenneyhe@gmail.com>
2017-09-19 09:55:44 -0700

    add assertion for single user login id in parser and refactor pattern searching

--- mpmain.py
+++ mpmain.py
@@ -4,7 +4,6 @@
 # developed by kenneyhe@gmail.com (2017)
 #
 
-
 import sys
 import os
 import main
@@ -13,7 +12,6 @@ import re
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "pkg"))
 sys.path.append(os.path.join(os.path.dirname(__file__), "pkg", "queryGenerators"))
-
 
 from config import Config
 
@@ -25,10 +23,21 @@ PATTERN_REF = re.compile(r'<account ref.*Live_.*</account> |.*', re.MULTILINE)
 
 
 def run_v1(config):
+    """
+    run 3.x script's main.__run(config)
+    :param config:
+    :return:
+    """
     return main.__run(config)
 
 
 def run(nodes=1, filename="config.xml"):
+    """
+    Dispatch Wrapper to run xml by dispatching each login to a multiprocess pool
+    :param nodes: sys argument 1
+    :param filename: (determine) by sys argument 2
+    :return: array of status of pool
+    """
     file_xml = ""
     with open(filename, "r") as fd:
         lines = fd.readlines()
@@ -37,51 +46,48 @@ def run(nodes=1, filename="config.xml"):
     xml = " ".join(file_xml.rsplit())
 
     pool = ProcessingPool(nodes)
-    runlist = [[xml, value] for value in PATTERN_SEL.findall(xml)]
-    return pool.map(helper, runlist)
+    run_list = [[xml, value] for value in PATTERN_SEL.findall(xml)]
+    return pool.map(helper, run_list)
 
 
 def replace(xml, selector):
     """
     replace xml accounts with only selector inside
-    :param xml:
-    :param selector:
-    :return:
+    :param xml: original xml
+    :param selector: login user
+    :return: xml file for just login user
     """
-    newXML = " ".join(xml.rsplit())
-    accounts = PATTERN_ACCS.findall(newXML)[0]
+    new_xml = " ".join(xml.rsplit())
+    accounts = PATTERN_ACCS.findall(new_xml)[0]
     type = accounts.split(">")[0] + ">"
-    for account in PATTERN_ACC.findall(accounts):
-        if selector in account:
-            break
-    newXML = PATTERN_ACCS.sub('<accounts> ' + type + account + "</account> </accounts>", newXML)
+    found = [account for account in PATTERN_ACC.findall(accounts) if selector in account]
+    assert len(found) == 1, "should be one and only one account login"
+    new_xml = PATTERN_ACCS.sub('<accounts> ' + type + found[0] + "</account> </accounts>", new_xml)
     try:
-        accounts = PATTERN_REFS.findall(newXML)[0]
-        for account in PATTERN_REF.findall(accounts):
-            if selector in account:
-                break
-        newXML = PATTERN_REFS.sub(account, newXML)
+        accounts = PATTERN_REFS.findall(new_xml)[0]
+        found = [account for account in PATTERN_REF.findall(accounts) if selector in account]
+        new_xml = PATTERN_REFS.sub(found[0], new_xml)
     except IndexError, e:
-        # do not care if no ref
+        # do not care if ref is optional
         pass
-    return newXML
+    return new_xml
 
 
 def helper(args):
     """
-    return XML string for running just that account
+    run just that account on main.__run(config object)
     :param XMLString: XML with all account
     :param selector:
-    :return:
+    :return:  XML string for running just that account
     """
     class runnable:
         def __init__(self):
             self.config = Config()
 
     [xml, selector] = args
-    newXML = replace(xml, selector)
+    new_xml = replace(xml, selector)
     run = runnable()
-    run.config.parseFromString(newXML)
+    run.config.parseFromString(new_xml)
     run_v1(run.config)
 
 if __name__ == "__main__":
