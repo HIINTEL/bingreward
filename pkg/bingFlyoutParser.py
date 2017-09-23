8d79ec18079df7e8e0bce6bae76c60730451473f
Kenney He <kenneyhe@gmail.com>
2017-09-23 12:59:01 -0700

    add gold sweep handler to bfp

--- pkg/bingFlyoutParser.py
+++ pkg/bingFlyoutParser.py
@@ -65,10 +65,10 @@ class Reward:
         SEARCH_AND_EARN      = (14,   "Search and earn",                   None, False, Action.SEARCH)
         THURSDAY_BONUS       = (15,   "Thursday bonus",                    None, False, Action.PASS)
         RE_QUIZ              = (16,   re.compile(r"\b[Qq]uiz\b"),          None, True,  Action.PASS)
-
+        GOLD_SWEEP           = (17,   "Xbox Live Gold",                    None, False, Action.PASS)
         ALL = (RE_EARN_CREDITS_PASS, RE_EARN_CREDITS, SEARCH_MOBILE, SEARCH_PC, YOUR_GOAL, MAINTAIN_GOLD,
                REFER_A_FRIEND, SEND_A_TWEET, RE_EARNED_CREDITS, COMPLETED, SILVER_STATUS, INVITE_FRIENDS,
-               EARN_MORE_POINTS, SEARCH_AND_EARN, THURSDAY_BONUS, RE_QUIZ)
+               EARN_MORE_POINTS, SEARCH_AND_EARN, THURSDAY_BONUS, RE_QUIZ, GOLD_SWEEP)
 
     def __init__(self):
         self.url = ""               # optional
